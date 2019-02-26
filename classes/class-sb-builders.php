e6118f25bd89b08ac3d39d41b5280969b99c30d2
raiden808 <raiden.nalasa@gmail.com>
2019-02-27 06:37:08 +0800

    added extra builder

--- classes/class-sb-builders.php
+++ classes/class-sb-builders.php
@@ -15,8 +15,11 @@ class SB_Builders{
 		?>
 		<h2>HTML</h2>
     	<textarea id="sb_build_html" name="sb_build_html"></textarea>
+		<h2>JS</h2>
+    	<textarea id="sb_build_js"   name="sb_build_js"></textarea>
+		<h2>CSS</h2>
+    	<textarea id="sb_build_css"  name="sb_build_css"></textarea>
 		<?php
-
 		$this->sb_builder_mirror_init();
 	}
 
@@ -31,7 +34,7 @@ class SB_Builders{
 				               mode: "vbscript"}]
 			};
 
-			var a = ["sb_build_html"];
+			var a = ["sb_build_html","sb_build_js","sb_build_css"];
 			var x = 0;
 
 			a.forEach(function(entry) {
