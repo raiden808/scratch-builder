2b053426546a64470ea9a103934b3bcf9de318f1
raiden808 <raiden.nalasa@gmail.com>
2019-02-26 08:53:02 +0800

    organize js

--- classes/class-sb-scripts.php
+++ classes/class-sb-scripts.php
@@ -11,16 +11,22 @@ class SB_Scripts{
 
 		//restrict build js
 		if(in_array($screen->post_type, $supported_post_type)){
-			
-			//base css
-			wp_enqueue_style( 'sb-mirror-style', SB_PLUGIN_URL . 'assets/code_mirror/css/codemirror.css' );
-			
+
 			//base js
 			wp_enqueue_script( 'sb-mirror-main', SB_PLUGIN_URL . 'assets/code_mirror/js/codemirror.js' );
+
+			//extension js
 			wp_enqueue_script( 'sb-mirror-xml', SB_PLUGIN_URL . 'assets/code_mirror/js/xml.js' );
 			wp_enqueue_script( 'sb-mirror-active', SB_PLUGIN_URL . 'assets/code_mirror/js/active-line.js' );
+			wp_enqueue_script( 'sb-mirror-active', SB_PLUGIN_URL . 'assets/code_mirror/js/active-line.js' );
 
-			
+			//base css
+			wp_enqueue_style( 'sb-mirror-style', SB_PLUGIN_URL . 'assets/code_mirror/css/codemirror.css' );
+
+			//theme css
+			wp_enqueue_style( 'sb-mirror-theme', SB_PLUGIN_URL . 'assets/code_mirror/css/base16-dark.css' );
+			wp_enqueue_style( 'sb-mirror-ocean', SB_PLUGIN_URL . 'assets/code_mirror/css/oceanic-next.css' );
+		
 		}
 	}
 }
