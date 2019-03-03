4e9fa0a4b02fa47729311c67e8acefc6e83d03e9
raiden808 <raiden.nalasa@gmail.com>
2019-03-04 07:27:52 +0800

    added shortcode display

--- classes/class-sb-display.php
+++ classes/class-sb-display.php
@@ -6,18 +6,26 @@ if (!defined('ABSPATH')) {
 
 class SB_Display{
 
-	function sb_build_display($atts = []){
+	function sb_build_display($atts = [], $content = null){
 		
 		$a = shortcode_atts(array( 
 		'build_id' =>  ''),$atts );
 
+		$build_id = esc_attr($a['build_id']);
+
 		ob_start();
 
-		
+		echo get_post_meta( $build_id, 'sb_build_css', true );
+
+		$build_html = get_post_meta( $build_id, 'sb_build_html', true );
+
+		//allow shortcode to be called inside the scratch html body
+		echo do_shortcode(sprintf($build_html,do_shortcode($content)));
+
+		echo get_post_meta( $build_id, 'sb_build_js', true );
 
 		return ob_get_clean();
 	}
-
 }
 
 new SB_Display;
