38fde03ce2c0bd73372921aa673f46eee123f438
raiden808 <raiden.nalasa@gmail.com>
2019-03-06 06:16:52 +0800

    added taxonomy

--- classes/class-sb-post-type.php
+++ classes/class-sb-post-type.php
@@ -6,6 +6,7 @@ if (!defined('ABSPATH')){
 class SB_Post_Type{
 	function __construct() {
 	    add_action('init',array($this,'scratch_builder_postype'));
+	    add_action('init',array($this,'scratch_builder_taxonomy'));
 	}
 	function scratch_builder_postype() {
 
@@ -60,7 +61,44 @@ class SB_Post_Type{
 			'publicly_queryable'    => true,
 			'capability_type'       => 'page',
 		);
+
 		register_post_type( 'scratch_builder', $args );
+	}
+
+	// Register Custom Taxonomy
+	function scratch_builder_taxonomy() {
+
+		$labels = array(
+			'name'                       => _x( 'Build Types', 'Taxonomy General Name', 'text_domain' ),
+			'singular_name'              => _x( 'Build Type', 'Taxonomy Singular Name', 'text_domain' ),
+			'menu_name'                  => __( 'Build Types', 'text_domain' ),
+			'all_items'                  => __( 'All Build Types', 'text_domain' ),
+			'parent_item'                => __( 'Parent Type', 'text_domain' ),
+			'parent_item_colon'          => __( 'Parent Tyype:', 'text_domain' ),
+			'new_item_name'              => __( 'New Type Name', 'text_domain' ),
+			'add_new_item'               => __( 'Add New Type', 'text_domain' ),
+			'edit_item'                  => __( 'Edit Type', 'text_domain' ),
+			'update_item'                => __( 'Update Type', 'text_domain' ),
+			'view_item'                  => __( 'View Type', 'text_domain' ),
+			'separate_items_with_commas' => __( 'Separate Type with commas', 'text_domain' ),
+			'add_or_remove_items'        => __( 'Add or remove Type', 'text_domain' ),
+			'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+			'popular_items'              => __( 'Popular Types', 'text_domain' ),
+			'search_items'               => __( 'Search Types', 'text_domain' ),
+			'not_found'                  => __( 'Not Found', 'text_domain' ),
+			'no_terms'                   => __( 'No Types', 'text_domain' ),
+			'items_list'                 => __( 'Types list', 'text_domain' ),
+			'items_list_navigation'      => __( 'Types list navigation', 'text_domain' ),
+		);
+		$args = array(
+			'labels'                     => $labels,
+			'hierarchical'               => true,
+			'public'                     => false,
+			'show_ui'                    => true,
+			'show_admin_column'          => true,
+			'show_in_nav_menus'          => true,
+			'show_tagcloud'              => true,
+		);
 		register_taxonomy( 'build_type', array( 'scratch_builder' ), $args );
 	}
 }
